7cd6f0b02e25453800a30ba24a2f76f03344f3c6
David Spörri <davepermen@hotmail.com>
2021-04-09 18:14:35 +0200

    added first utility extension, UseHostingDefaults

---
+++ src/Conesoft.Hosting/IApplicationBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System.Linq;
+
+namespace Conesoft.Hosting
+{
+    public static class IApplicationBuilderExtensions
+    {
+        static readonly string[] contentTypes = new[] { "text", "json", "xml" };
+        public static IApplicationBuilder UseHostingDefaults(this IApplicationBuilder app, bool useDefaultFiles, bool useStaticFiles)
+        {
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("Content-Type", "text/html; charset=utf-8");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                await next.Invoke();
+            });
+
+            if (useDefaultFiles)
+            {
+                app.UseDefaultFiles();
+            }
+            if (useStaticFiles)
+            {
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    OnPrepareResponse = context =>
+                    {
+                        if (context.Context.Response.Headers["Content-Type"].Count > 0)
+                        {
+                            var contentType = context.Context.Response.Headers["Content-Type"][0];
+                            if (contentTypes.Any(type => contentType.Contains(type)))
+                            {
+                                context.Context.Response.Headers["Content-Type"] = contentType + "; charset=utf-8";
+                            }
+                        }
+                        if (context.Context.Response.Headers["Cache-Control"].Count == 0)
+                        {
+                            context.Context.Response.Headers.Add("Cache-Control", "max-age=31536000, immutable");
+                        }
+                    }
+                });
+            }
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Response.Headers["Cache-Control"].Count == 0)
+                {
+                    context.Response.Headers.Add("Cache-Control", "no-cache");
+                }
+                await next.Invoke();
+            });
+            return app;
+        }
+    }
+}
