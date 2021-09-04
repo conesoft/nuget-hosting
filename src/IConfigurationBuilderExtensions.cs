d857a2ebf3b17faed817bd54cdbd71b04c0601ef
David Spörri <davepermen@hotmail.com>
2021-09-04 18:24:23 +0200

    net6 minimal api support

---
+++ src/IConfigurationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Conesoft.Hosting
+{
+    public static class IConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddHostConfiguration(this IConfigurationBuilder configuration)
+        {
+            configuration.AddJsonFile(Host.LocalSettings.Path, optional: true, reloadOnChange: true);
+            configuration.AddJsonFile(Host.GlobalSettings.Path, optional: true, reloadOnChange: true);
+            return configuration;
+        }
+    }
+}
