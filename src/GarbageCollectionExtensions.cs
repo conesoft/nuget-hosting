687e060c9a3dac20059c2d5da18a125f7e9e3042
David Spörri <davepermen@hotmail.com>
2024-06-09 20:39:03 +0200

    yolo

---
+++ src/GarbageCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Conesoft.Hosting.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Conesoft.Hosting;
+
+public static class GarbageCollectionExtensions
+{
+    public static IServiceCollection AddPeriodicGarbageCollection(this IServiceCollection services, TimeSpan period)
+    {
+        return services.AddHostedServiceWith<GarbageCollect>(period);
+    }
+}
---
+++ src/Services/GarbageCollect.cs
@@ -0,0 +1,19 @@
+﻿using Humanizer;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace Conesoft.Hosting.Services;
+
+public class GarbageCollect(TimeSpan period) : PeriodicTask(period)
+{
+    protected override Task Process()
+    {
+        var memoryBefore = System.Diagnostics.Process.GetCurrentProcess().PrivateMemorySize64;
+        GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive, blocking: true, compacting: true);
+        var memoryAfter = System.Diagnostics.Process.GetCurrentProcess().PrivateMemorySize64;
+        Log.Information($"running garbage collection cycle: {Math.Max(0, memoryBefore - memoryAfter).Bytes()} released, {memoryAfter.Bytes()} in use");
+
+        return Task.CompletedTask;
+    }
+}
