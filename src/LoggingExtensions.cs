336fda53ff7cf42c1de41ce91c8a7c66f22a9a71
David Spörri <davepermen@hotmail.com>
2024-06-09 20:23:25 +0200

    ok

--- src/LoggingExtensions.cs
+++ src/LoggingExtensions.cs
@@ -1,15 +1,20 @@
 ﻿using Conesoft.Files;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Conesoft.Hosting;
 
 public static class LoggingExtensions
 {
+    private static string ToTitleCase(this string text) => System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.ToLowerInvariant());
+
     public static IServiceCollection AddLoggingToHost(this IServiceCollection services)
     {
-        var log = Host.Root / "Logs" / Filename.From($"{Host.HostingType} - {Host.Name.ToLowerInvariant()} - ", "txt");
+        var log = Host.Root / "Logs" / Filename.From($"{Host.HostingType} - {Host.Name.ToTitleCase()} - ", "txt");
 
         Log.Logger = new LoggerConfiguration()
             .WriteTo.Console()
@@ -22,8 +27,25 @@ public static class LoggingExtensions
             )
             .CreateLogger();
 
-        Log.Information($"app '{Host.Name}' starting up...");
+        Log.Information($"app '{Host.Name.ToTitleCase()}' starting up...");
+
+        services.AddHostedService<HostedLoggingExtension>();
 
         return services;
+    }
+
+    class HostedLoggingExtension : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Log.Information($"app '{Host.Name.ToTitleCase()}' started");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Log.Information($"app '{Host.Name.ToTitleCase()}' stopped");
+            return Task.CompletedTask;
+        }
     }
 }
