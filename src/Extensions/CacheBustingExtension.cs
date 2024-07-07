aa83c6e5beba7962f6f82a98851026672e0fb8ff
David Spörri <davepermen@hotmail.com>
2024-07-07 17:15:28 +0200

    dfs

--- src/Extensions/CacheBustingExtension.cs
+++ src/Extensions/CacheBustingExtension.cs
@@ -10,7 +10,7 @@ public static class CacheBustingExtension
 {
     public static IServiceCollection AddCompiledHashCacheBuster(this IServiceCollection services) => services.AddSingleton<ApplicationBuildHash>();
 
-    public static IApplicationBuilder UseCompiledHashCacheBuster(this IApplicationBuilder app, string cookieName) => app.Use(async (context, next) =>
+    public static IApplicationBuilder UseCompiledHashCacheBuster(this IApplicationBuilder app, string cookieName = "Conesoft.ApplicationHash") => app.Use(async (context, next) =>
     {
         var abh = (context.RequestServices.GetService(typeof(ApplicationBuildHash)) as ApplicationBuildHash)!;
         if (context.Request.Cookies.ContainsKey(cookieName) == true)
