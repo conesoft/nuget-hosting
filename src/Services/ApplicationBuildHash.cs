c55dd744e2f3101bcb904df7579ba812ab6546ef
David Spörri <davepermen@hotmail.com>
2024-07-16 19:37:38 +0200

    ok

--- src/Services/ApplicationBuildHash.cs
+++ src/Services/ApplicationBuildHash.cs
@@ -1,17 +1,26 @@
 ﻿using Serilog;
 using System;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace Conesoft.Hosting.Services;
 
 public class ApplicationBuildHash
 {
-    private int? compiledHash;
-    public int CompiledHash => compiledHash ??= RetrieveLinkerHash();
-    public string CompiledHashString => CompiledHash.ToString("X");
+    private string? compiledHash;
+    public string CompiledHash => compiledHash ??= RetrieveLinkerHash();
+
 
     // http://www.codinghorror.com/blog/2005/04/determining-build-date-the-hard-way.html
-    private static int RetrieveLinkerHash()
+
+    private static string RetrieveLinkerHash()
+    {
+        Log.Information("generating app hash for {assembly}", Assembly.GetEntryAssembly()?.Location);
+        return string.Join("", AppDomain.CurrentDomain.GetAssemblies().Select(RetrieveLinkerHash).Select(h => $"{h:X}"));
+    }
+
+    private static string RetrieveLinkerHash(Assembly assembly)
     {
         const int peHeaderOffset = 60;
         const int linkerCompileHashOffset = 8;
@@ -19,16 +28,16 @@ public class ApplicationBuildHash
         FileStream? s = null;
         try
         {
-            Log.Information("generating app hash for {application}", System.Reflection.Assembly.GetEntryAssembly()?.Location);
-            s = new FileStream(System.Reflection.Assembly.GetEntryAssembly()?.Location!, FileMode.Open, FileAccess.Read);
+            Log.Information("generating assembly hash for {assembly}", assembly?.Location);
+            s = new FileStream(Assembly.GetEntryAssembly()?.Location!, FileMode.Open, FileAccess.Read);
             s.Read(b, 0, 2048);
         }
         finally
         {
             s?.Close();
         }
-        var hash = BitConverter.ToInt32(b, BitConverter.ToInt32(b, peHeaderOffset) + linkerCompileHashOffset);
-        Log.Information($"current app hash: {hash:X}");
+        var hash = $"{BitConverter.ToInt32(b, BitConverter.ToInt32(b, peHeaderOffset) + linkerCompileHashOffset):X}";
+        Log.Information("assembly hash for {assembly}: {hash}", assembly?.Location, hash);
         return hash;
     }
 }
