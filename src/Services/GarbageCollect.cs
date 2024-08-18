702ba6cc141d93b23deed9978394ea78785dd5bb
David Spörri <davepermen@hotmail.com>
2024-08-18 19:09:05 +0200

    ok then

--- src/Services/GarbageCollect.cs
+++ src/Services/GarbageCollect.cs
@@ -1,5 +1,4 @@
-﻿using Humanizer;
-using Serilog;
+﻿using Serilog;
 using System;
 using System.Threading.Tasks;
 
@@ -14,13 +13,10 @@ public class GarbageCollect(TimeSpan period) : PeriodicTask(period)
         var memoryBefore = System.Diagnostics.Process.GetCurrentProcess().PrivateMemorySize64;
         GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive, blocking: true, compacting: true);
         var memoryAfter = System.Diagnostics.Process.GetCurrentProcess().PrivateMemorySize64;
-        Log.Information("running garbage collection cycle: {releasedMemory} released, {usedMemory} in use", new Bytes(Math.Max(0, memoryBefore - memoryAfter)), new Bytes(memoryAfter));
+        Log.Information("running garbage collection cycle: {@releasedMemory} released, {@usedMemory} in use", new Bytes(Math.Max(0, memoryBefore - memoryAfter)), new Bytes(memoryAfter));
 
         return Task.CompletedTask;
     }
-
-    record struct Bytes(long Value)
-    {
-        public override readonly string ToString() => Value.Bytes().ToString();
-    };
+    record struct Bytes(long Value);
 }
+
