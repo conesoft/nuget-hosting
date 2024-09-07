37b74f39e3970d0f508f19737846354ad2098800
David Spörri <davepermen@hotmail.com>
2024-09-07 15:33:39 +0200

    oh

--- src/Extensions/LoggingExtensions.cs
+++ src/Extensions/LoggingExtensions.cs
@@ -9,7 +9,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Conesoft.Hosting.Extensions;
+namespace Conesoft.Hosting;
 
 public static class LoggingExtensions
 {
