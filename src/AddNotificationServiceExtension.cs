40107a2bcdd189f5e027f35894aa880dc86cabb6
David Spörri <davepermen@hotmail.com>
2025-01-02 19:44:48 +0100

    add notifications support

---
+++ src/AddNotificationServiceExtension.cs
@@ -0,0 +1,16 @@
+﻿using Conesoft.Notifications;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel;
+
+namespace Conesoft.Hosting;
+
+[Browsable(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class AddNotificationServiceExtension
+{
+    public static Builder AddNotificationService<Builder>(this Builder builder) where Builder : IHostApplicationBuilder
+    {
+        builder.AddNotificationService((HostEnvironment environment) => environment.Global.Storage);
+        return builder;
+    }
+}
