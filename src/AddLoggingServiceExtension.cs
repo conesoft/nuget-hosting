1712717b534f1202e990890c08e5c91c830b073c
David Spörri <davepermen@hotmail.com>
2025-01-12 14:49:15 +0100

    maybe (:

--- src/AddLoggingServiceExtension.cs
+++ src/AddLoggingServiceExtension.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Serilog;
+using System;
 using System.ComponentModel;
 
 namespace Conesoft.Hosting;
@@ -12,7 +15,11 @@ public static partial class AddLoggingServiceExtension
 {
     public static Builder AddLoggingService<Builder>(this Builder builder) where Builder : IHostApplicationBuilder
     {
-        AttachConsole(-1);
+        var environment = CreateHostEnvironmentFromConfigurationManually(builder.Configuration);
+        if(environment.IsInHostedEnvironment == false)
+        {
+            AttachConsole(-1);
+        }
         builder.Services.AddSingleton<LoggingService>();
         builder.Services.AddHostedService(s => s.GetRequiredService<LoggingService>()).AddSerilog();
         return builder;
@@ -28,4 +35,15 @@ public static partial class AddLoggingServiceExtension
     [System.Runtime.InteropServices.LibraryImport("kernel32.dll")]
     [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
     static private partial bool AttachConsole(int dwProcessId);
+
+    private static HostEnvironment CreateHostEnvironmentFromConfigurationManually(IConfigurationManager configurationManager)
+    {
+        // can't use DI yet, but can access configuration already.
+        var options = configurationManager.GetRequiredSection("hosting").Get<HostingOptions>();
+        if (options == null)
+        {
+            throw new Exception("can't read the required hosting section");
+        }
+        return new HostEnvironment(Options.Create(options));
+    }
 }
